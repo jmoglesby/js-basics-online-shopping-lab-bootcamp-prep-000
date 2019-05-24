f9fc99004a124bd15a140f114a4a305c9655e78c
Jeremy Oglesby <iaminaband@gmail.com>
2019-05-24 15:51:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,37 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 10) + 1;
+ cart.push({itemName: item, itemPrice: price});
+ 
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  var res = "In your cart, you have ";
+  if (cart.length > 0) {
+    for (let i = 0; i < cart.length; i++) {
+      var singleItem = (cart.length == 1);
+      var finalItem = (i == cart.length - 1);
+      var item = cart[i].itemName;
+      var price = cart[i].itemPrice;
+      
+      res += `${!singleItem & finalItem ? 'and ' : ''}${item} at $${price}${finalItem ? '.' : ', '}`;
+    }// end for loop
+  } else {
+    res = "Your shopping cart is empty."
+  }// end if-else
+  return res;
 }
 
 function total() {
   // write your code here
+  var total = 0;
+  for (let i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
